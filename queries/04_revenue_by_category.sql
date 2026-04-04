9a72f4e61b9de15ee9070e131104a21252ccc08c
Lina Liu <82733958+lliu6907@users.noreply.github.com>
2026-04-03 21:42:45 -0400

    Update 04_revenue_by_category.sql

--- queries/04_revenue_by_category.sql
+++ queries/04_revenue_by_category.sql
@@ -1,1 +1,19 @@
+-- Top 5 product categories that drove revenue
+SELECT
+  item.item_category,
+  SUM(item.item_revenue_in_usd) AS revenue_usd
+FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
+UNNEST(items) AS item
+WHERE event_name = 'purchase' AND item.item_name <> '(not set)'
+GROUP BY item.item_category
+ORDER BY revenue_usd DESC
+LIMIT 5
 
+-- Result
+-- Apparel: $171,727
+-- New: $25,813
+-- Bags: $23,860
+-- Campus Collection: $20,061
+-- Accessories: $17,815
+
+-- Insight: The top three product categories that drove money consisted of apparel, new, and bags with $171,727, $25,813, and $23,860 of revenue, respectively. Apparel made up 66.2% of revenue compared to the other 4.
