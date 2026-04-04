951872cdf47f98613ff4c652e488bef6b33b641b
Lina Liu <82733958+lliu6907@users.noreply.github.com>
2026-04-03 21:22:35 -0400

    Update 03_top_products_by_revenue.sql

--- queries/03_top_products_by_revenue.sql
+++ queries/03_top_products_by_revenue.sql
@@ -2,16 +2,15 @@
 
 SELECT
  item.item_name,
- SUM(item.item_revenue_in_usd) AS revenue_usd,
- COUNT(*) AS purchase_count
+ SUM(item.item_revenue_in_usd) AS revenue_usd
 FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
 UNNEST(items) AS item
 WHERE event_name = 'purchase' AND item.item_name <> '(not set)'
 GROUP BY item.item_name
-ORDER BY purchase_count DESC
+ORDER BY revenue_usd DESC
 LIMIT 3;
 
 -- Result:
 -- 1. Google Zip Hoodie F/C ($13,788)
 -- 2. Google Crewneck Sweatshirt Navy ($10,714)
--- 3. Super G Unisex Joggers ($9,529)
+-- 3. Google Men’s Tech Fleece Grey ($9,965)
