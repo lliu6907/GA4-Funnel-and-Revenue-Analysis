66a794775950f0c4e43108d28c8b77c95f89ec35
Lina Liu <82733958+lliu6907@users.noreply.github.com>
2026-04-03 21:13:28 -0400

    Update 01_top_products_by_purchase_count.sql

--- queries/01_top_products_by_purchase_count.sql
+++ queries/01_top_products_by_purchase_count.sql
@@ -1,1 +1,18 @@
+-- Identify the top 3 purchased products by purchase count
 
+SELECT
+  item.item_name,
+  COUNT(*) AS purchase_count
+FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
+UNNEST(items) AS item
+WHERE event_name = 'purchase'
+  AND item.item_name IS NOT NULL
+  AND item.item_name <> '(not set)'
+GROUP BY item.item_name
+ORDER BY purchase_count DESC
+LIMIT 3;
+
+-- Result:
+-- 1. Super G Unisex Joggers
+-- 2. Google Zip Hoodie F/C
+-- 3. Google Crewneck Sweatshirt Navy
