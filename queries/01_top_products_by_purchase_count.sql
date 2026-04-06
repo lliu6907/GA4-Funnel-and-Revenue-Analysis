830f9a2e3a12af17efe097e89df22a1c4c199c56
Lina Liu <82733958+lliu6907@users.noreply.github.com>
2026-04-06 19:51:41 -0400

    Fixed code error

--- queries/01_top_products_by_purchase_count.sql
+++ queries/01_top_products_by_purchase_count.sql
@@ -1,18 +1,16 @@
 -- Identify the top 3 purchased products by purchase count
 
 SELECT
-  item.item_name,
-  COUNT(*) AS purchase_count
+item.item_name,
+SUM(item.quantity) AS purchase_count
 FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
 UNNEST(items) AS item
-WHERE event_name = 'purchase'
-  AND item.item_name IS NOT NULL
-  AND item.item_name <> '(not set)'
+WHERE event_name = 'purchase' AND item.item_name <> '(not set)'
 GROUP BY item.item_name
 ORDER BY purchase_count DESC
 LIMIT 3;
 
 -- Result:
--- 1. Super G Unisex Joggers
--- 2. Google Zip Hoodie F/C
--- 3. Google Crewneck Sweatshirt Navy
+-- 1. Google Clear Pen 4-Pack
+-- 2. Google Laptop and Cell Phone Stickers
+-- 3. Google Metallic Notebook Set
