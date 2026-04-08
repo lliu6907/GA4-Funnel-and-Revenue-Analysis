4a451ba6fa5c6bc87b53236a4c59a161e43bcec3
Lina Liu <82733958+lliu6907@users.noreply.github.com>
2026-04-07 23:34:36 -0400

    Update 05_funnel_distinct_users.sql

--- queries/05_funnel_distinct_users.sql
+++ queries/05_funnel_distinct_users.sql
@@ -1,10 +1,63 @@
 -- Funnel drop-off. When do users stop purchasing?
+WITH base AS (
+  SELECT
+    user_pseudo_id,
+    event_name,
+    event_timestamp
+  FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+  WHERE event_name IN ('view_item', 'add_to_cart', 'begin_checkout', 'purchase')
+),
+
+view_step AS (
+  SELECT
+    user_pseudo_id,
+    MIN(event_timestamp) AS view_ts
+  FROM base
+  WHERE event_name = 'view_item'
+  GROUP BY user_pseudo_id
+),
+
+cart_step AS (
+  SELECT
+    b.user_pseudo_id,
+    MIN(b.event_timestamp) AS cart_ts
+  FROM base b
+  JOIN view_step v
+    ON b.user_pseudo_id = v.user_pseudo_id
+  WHERE b.event_name = 'add_to_cart'
+    AND b.event_timestamp > v.view_ts
+  GROUP BY b.user_pseudo_id
+),
+
+checkout_step AS (
+  SELECT
+    b.user_pseudo_id,
+    MIN(b.event_timestamp) AS checkout_ts
+  FROM base b
+  JOIN cart_step c
+    ON b.user_pseudo_id = c.user_pseudo_id
+  WHERE b.event_name = 'begin_checkout'
+    AND b.event_timestamp > c.cart_ts
+  GROUP BY b.user_pseudo_id
+),
+
+purchase_step AS (
+  SELECT
+    b.user_pseudo_id,
+    MIN(b.event_timestamp) AS purchase_ts
+  FROM base b
+  JOIN checkout_step AS ch
+    ON b.user_pseudo_id = ch.user_pseudo_id
+  WHERE b.event_name = 'purchase'
+    AND b.event_timestamp > ch.checkout_ts
+  GROUP BY b.user_pseudo_id
+)
+
 SELECT
- event_name,
- COUNT(DISTINCT(user_pseudo_id)) AS event
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
-WHERE event_name IN ('view_item', 'add_to_cart', 'begin_checkout', 'purchase')
-GROUP BY event_name;
+  (SELECT COUNT(*) FROM view_step) AS viewed,
+  (SELECT COUNT(*) FROM cart_step) AS added_to_cart,
+  (SELECT COUNT(*) FROM checkout_step) AS began_checkout,
+  (SELECT COUNT(*) FROM purchase_step) AS purchased
 
 -- Result
 -- view_item: 61,252
